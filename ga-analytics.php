6aaf9b5b0d40079367ba4166fb34681302003da8
Will Haynes <haynes24@gmail.com>
2014-11-24 06:37:39 -0600

    Use new list approach for popular posts.

--- ga-analytics.php
+++ ga-analytics.php
@@ -130,7 +130,7 @@ register_deactivation_hook(__FILE__, 'analytic_bridge_plugin_deinit');
  */
 function new_interval($interval) {
 
-    $interval['10m'] = array('interval' => 1*60, 'display' => 'Once 10 minutes');
+    $interval['10m'] = array('interval' => 20*60, 'display' => 'Once 10 minutes');
     return $interval;
 
 }
@@ -680,137 +680,14 @@ function analyticbridge_popular_posts_widget() {
 	// Display whatever it is you want to show.
 	echo "<p>Most popular posts from Google Analytics, with a relative weighting average.</p>";
 
-
-	// 1: Calculate a ratio coeffient 
-	$tday = new DateTime('today',new DateTimeZone('America/Chicago'));
-	$now = new DateTime('',new DateTimeZone('America/Chicago'));
-
-	$interval = $tday->diff($now);
-
-	$minutes = $interval->h * 60 + $interval->i;
-
-	$ratio = $minutes / (24*60);
-
-	/* sql statement that pulls todays sessions, yesterdays 		*/
-	/* sessions and a weighted average of them from the database.	*/
-
-	$halflife = 14;
-
-	$result = $wpdb->get_results("
-
-		--							---
-		--  SELECT POPULAR POSTS 	---
-		--							---
-
-		SELECT
-
-			pg.pagepath AS pagepath, 
-			pg.id AS page_id,
-			pst.id AS post_id, 
-			coalesce(t.sessions, 0) AS today_pageviews, 
-			coalesce(y.sessions, 0) AS yesterday_pageviews, 
-
-			-- calculate the weighted session averages.
-
-			( -- Calculate avg_pageviews.
-				(coalesce(t.sessions, 0) * $ratio) + 
-				(coalesce(y.sessions, 0) * (1 - $ratio))
-			) AS `avg_pageviews`,
-			
-			( -- Calulate days_old
-				TIMESTAMPDIFF( hour, pst.post_date, NOW() ) - 1
-			) AS `days_old`,
-			
-			( -- Calculate weighted_pageviews
-				(
-					(coalesce(t.sessions, 0) * $ratio * .4) + 
-					(coalesce(y.sessions, 0) * (1 - $ratio))
-				) * POWER( 
-					1/2, 
-					( TIMESTAMPDIFF( hour, pst.post_date, NOW() ) - 1 ) / ($halflife * 24)
-				)
-			) AS `weighted_pageviews`
-
-		FROM 
-
-			`" . PAGES_TABLE . "` as `pg`
-
-		LEFT JOIN (
-		
-			-- 
-			-- Nested select returns today's sessions.
-			--
-		
-			SELECT
-
-				CAST(value as unsigned) as `sessions`, 
-				page_id
-			
-			FROM
-
-				`" . METRICS_TABLE . "` as m
-			
-			WHERE
-
-				m.metric = 'ga:pageviews'
-			
-			AND
-
-				m.startdate >= CURDATE() 
-		
-		) as `t` ON pg.id = t.page_id
-		
-		LEFT JOIN (
-		
-			-- 
-			-- Nested select returns yesterday's sessions.
-			--
-		
-			SELECT 
-			
-				CAST(value as unsigned) as `sessions`, 
-				`page_id`
-			
-			FROM
-			
-				`" . METRICS_TABLE . "` as m
-			
-			WHERE
-			
-				m.metric = 'ga:pageviews'
-			
-			AND 
-			
-				m.startdate >= CURDATE() - 1
-			
-			AND
-			
-				m.enddate < CURDATE()
-		
-		
-		) as `y` ON `pg`.`id` = `y`.`page_id`
-		
-		LEFT JOIN `" . $wpdb->prefix . "posts` as `pst`
-		  
-			ON `pst`.`id` = `pg`.`post_id`
-
-		-- For now, they must be posts.
-
-		WHERE `pst`.`post_type` = 'post'
-			
-			ORDER BY `weighted_pageviews` DESC
-			LIMIT 50
-
-
-	");
-
+	$popPosts = new AnayticBridgeList();
 
 	// 3: Print list
 	
 	echo "<table>";
 	$i = 1;
 	
-	foreach($result as $r) {
+	foreach($popPosts as $r) {
 		echo "<tr>";
 			echo "<td rowspan='2'>#" . $i . "</td>";
 			echo "<td><b>" . get_the_title($r->post_id) . "</b><em> (" . $r->days_old . " hours ago)</em></td>";
@@ -951,6 +828,8 @@ Class AnayticBridgeList implements Iterator {
 	// half life to use while querying.
 	private $halflife;
 
+	private $size;
+
 	// interval to query over.
 	private $interval;
 
@@ -959,6 +838,7 @@ Class AnayticBridgeList implements Iterator {
 		$this->position = 0;
 		$this->queried = false;
 		$this->halflife = 14;
+		$this->size = 20;
 
     }
 
@@ -979,8 +859,6 @@ Class AnayticBridgeList implements Iterator {
 		/* sql statement that pulls todays sessions, yesterdays 		*/
 		/* sessions and a weighted average of them from the database.	*/
 
-		$halflife = 14;
-
     	$this->result = $wpdb->get_results("
 
 			--							---
@@ -998,7 +876,7 @@ Class AnayticBridgeList implements Iterator {
 				-- calculate the weighted session averages.
 
 				( -- Calculate avg_pageviews.
-					(coalesce(t.sessions, 0) * $ratio) + 
+					(coalesce(t.sessions, 0) * $ratio * .5) + 
 					(coalesce(y.sessions, 0) * (1 - $ratio))
 				) AS `avg_pageviews`,
 				
@@ -1008,11 +886,11 @@ Class AnayticBridgeList implements Iterator {
 				
 				( -- Calculate weighted_pageviews
 					(
-						(coalesce(t.sessions, 0) * $ratio) + 
+						(coalesce(t.sessions, 0) * $ratio * .5) + 
 						(coalesce(y.sessions, 0) * (1 - $ratio))
 					) * POWER( 
 						1/2, 
-						( TIMESTAMPDIFF( hour, pst.post_date, NOW() ) - 1 ) / ($halflife * 24)
+						( TIMESTAMPDIFF( hour, pst.post_date, NOW() ) - 1 ) / ($this->halflife * 24)
 					)
 				) AS `weighted_pageviews`
 
@@ -1084,7 +962,7 @@ Class AnayticBridgeList implements Iterator {
 			WHERE `pst`.`post_type` = 'post'
 				
 				ORDER BY `weighted_pageviews` DESC
-				LIMIT 50
+				LIMIT $this->size
 
 
 		");
@@ -1098,7 +976,7 @@ Class AnayticBridgeList implements Iterator {
 	public function rewind() {
 
 		if( !$this->queried ) {
-			query();
+			$this->query();
 		}
 		$this->position = 0;
 
@@ -1107,7 +985,7 @@ Class AnayticBridgeList implements Iterator {
 	public function current() {
 
 		if( !$this->queried ) {
-			query();
+			$this->query();
 		}
 
 		return $this->result[$this->position];
@@ -1118,7 +996,7 @@ Class AnayticBridgeList implements Iterator {
 	public function key() {
 
 		if( !$this->queried ) {
-			query();
+			$this->query();
 		}
 
 		return $this->position;
@@ -1128,7 +1006,7 @@ Class AnayticBridgeList implements Iterator {
 	public function next() {
 
 		if( !$this->queried ) {
-			query();
+			$this->query();
 		}
 
 		$this->position += 1;
@@ -1140,12 +1018,11 @@ Class AnayticBridgeList implements Iterator {
 	public function valid() {
 
 		if( !$this->queried ) {
-			query();
+			$this->query();
 		}
 
 		return isset($this->result[$this->position]);
 		
 	}
-
 
 }
