5b0e7a18f1de28591614a0c72e478148830b4815
Ryan Nagle <rmnagle@gmail.com>
2016-01-04 17:08:29 -0600

    tests aren't doing much rn, but they were failing and i felt compelled to fix >_<

--- tests/classes/test-AnalyticsPopularWidget.php
+++ tests/classes/test-AnalyticsPopularWidget.php
@@ -5,8 +5,8 @@ class AnalyticBridgePopularPostWidget_Test extends WP_UnitTestCase {
 		parent::setUp();
 	}
 
-	function test_registered() {
-		var_log( $GLOBALS['wp_widget_factory']->widgets['WP_Widget_RSS'] );
-		var_log( $GLOBALS['wp_widget_factory']->widgets['AnalyticBridgePopularPostWidget'] );
+	function testSample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
 	}
 }
