193fd8df64f64d5b79edc3a0fda4f18e7d66d668
Adam Schweigert <aschweigert@gmail.com>
2016-06-09 13:31:26 -0400

    add links to posts in the dashboard widget

--- classes/AnalyticsDashWidget.php
+++ classes/AnalyticsDashWidget.php
@@ -23,7 +23,7 @@ add_action( 'wp_dashboard_setup', 'analyticbridge_add_dashboard_widgets' );
 /**
  * Outputs the HTML for the popular post widget.
  *
- * An unordered list of 20 popular posts.
+ * An unordered list of 5 popular posts.
  *
  * @since 0.1
  */
@@ -32,29 +32,29 @@ function analyticbridge_popular_posts_widget() {
 	global $wpdb;
 
 	// Display whatever it is you want to show.
-	echo "<p>Most popular posts from Google Analytics, with a relative weighting average.</p>";
+	echo '<p>Most popular posts from Google Analytics, with a relative weighting average.</p>';
 
 	$popPosts = new AnayticBridgePopularPosts();
-	$popPosts->size = "5";
+	$popPosts->size = '5';
 
 	// 3: Print list
 	
-	echo "<table>";
+	echo '<table>';
 	$i = 1;
 	
-	foreach($popPosts as $r) {
-		echo "<tr>";
-			echo "<td rowspan='2'>#" . $i . "</td>";
-			echo "<td><b>" . get_the_title($r->post_id) . "</b><em> (" . $r->days_old . " hours ago)</em></td>";
-		echo "</tr>";
-		echo "<tr>";
-			echo "<td style='color:#939393'><em>yesterday: " . $r->yesterday_pageviews . " views, ";
-			echo "today: " . $r->today_pageviews . " views, ";
-			echo "<b>weight:</b> " . number_format($r->weighted_pageviews,2) . "</em></td>";
-		echo "</tr>";
+	foreach( $popPosts as $r ) {
+		echo '<tr>';
+			echo '<td rowspan="2">#' . $i . '</td>';
+			echo '<td><a href="' . get_permalink( $r->post_id ) . '"><b>' . get_the_title( $r->post_id ) . '</b></a> <em>(' . $r->days_old . ' hours ago)</em></td>';
+		echo '</tr>';
+		echo '<tr>';
+			echo '<td style="color:#939393"><em>yesterday: ' . $r->yesterday_pageviews . ' views, ';
+			echo 'today: ' . $r->today_pageviews . ' views, ';
+			echo '<b>weight:</b> ' . number_format( $r->weighted_pageviews, 2 ) . '</em></td>';
+		echo '</tr>';
 		$i++;
 	}
 
-	echo "</table>";
+	echo '</table>';
 
 }
